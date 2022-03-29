504d41cf8a5c1f9379d7104d44cf48d8b91f5177
GrupieBug <emkerr00@gmail.com>
2022-03-28 18:02:35 -0600

    added paint event

--- SpriteEditor/mainwindow.cpp
+++ SpriteEditor/mainwindow.cpp
@@ -11,9 +11,21 @@ MainWindow::MainWindow(Model& model,QWidget *parent)
     QImage *img = &model.panel;
     ui->setupUi(this);
     ui->mainCanvas->setPixmap(QPixmap::fromImage(*img));
+
+    // button connections
+    connect(ui->mainCanvas, SIGNAL(valueChanged(int)),
+    this, SLOT(valuesChanged()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *) {
+ // Create a painter
+ QPainter painter(this);
+ QPen pen(Qt::black);
+ painter.setPen(pen);
+
 }
--- SpriteEditor/mainwindow.h
+++ SpriteEditor/mainwindow.h
@@ -19,5 +19,6 @@ public:
 private:
     Ui::MainWindow *ui;
     Canvas *canvas;
+    void paintEvent(QPaintEvent *);
 };
 #endif
